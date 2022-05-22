c7ffd3d09ec67ddd4faa1c02e28bf5cc274df4d6
OrangeKir <fraiming_king@outlook.com>
2022-05-22 18:30:27 +0300

    Fix build

--- service-layer/main.go
+++ service-layer/main.go
@@ -2,9 +2,38 @@ package main
 
 import (
 	gateway_layer "Backend/gateway-layer"
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
 )
+
+const dbConnectionString = "postgresql://postgres:1@localhost:5432/vcr"
+
+func AddDb() (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), dbConnectionString)
+	if err != nil {
+		conn.Close(context.Background())
+	}
+
+	return conn, err
+}
+
+func initZapLog() *zap.Logger {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	logger, err := config.Build()
+
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	return logger
+}
 
 func main() {
 	logger := initZapLog()
